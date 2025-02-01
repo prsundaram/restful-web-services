504e4a77a7935e39d8e581a0196a9f90719674d8
Prasanna Sundaram <prasannaa.sundaram@hotmail.com>
2025-02-01 16:11:28 +0530

    Added a simple get mapping with path variable

--- src/main/java/com/psundara/rest/webservices/restful_web_services/controller/HelloWorldController.java
+++ src/main/java/com/psundara/rest/webservices/restful_web_services/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.psundara.rest.webservices.restful_web_services.controller;
 
 import com.psundara.rest.webservices.restful_web_services.controller.model.HelloWorld;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +16,10 @@ public class HelloWorldController {
     @GetMapping("hello-world-bean")
     public HelloWorld helloWordBean() {
         return new HelloWorld("Hello World");
+    }
+
+    @GetMapping("hello-world/path-variable/{name}")
+    public HelloWorld helloWordBean(@PathVariable String name) {
+        return new HelloWorld(String.format("Hello World, %s", name));
     }
 }
