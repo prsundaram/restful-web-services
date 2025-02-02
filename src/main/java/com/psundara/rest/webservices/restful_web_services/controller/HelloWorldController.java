67414cfffe62614e52a8e04b48ead8eb17534c53
Prasanna Sundaram <prasannaa.sundaram@hotmail.com>
2025-02-02 21:39:22 +0530

    Added i18n support for simple endpoint

--- src/main/java/com/psundara/rest/webservices/restful_web_services/controller/HelloWorldController.java
+++ src/main/java/com/psundara/rest/webservices/restful_web_services/controller/HelloWorldController.java
@@ -1,12 +1,24 @@
 package com.psundara.rest.webservices.restful_web_services.controller;
 
 import com.psundara.rest.webservices.restful_web_services.model.HelloWorld;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    private final MessageSource messageSource;
+
+    @Autowired
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping("hello-world")
     public String helloWord() {
@@ -21,5 +33,11 @@ public class HelloWorldController {
     @GetMapping("hello-world/path-variable/{name}")
     public HelloWorld helloWordBean(@PathVariable String name) {
         return new HelloWorld(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping("hello-world-internationalized")
+    public String helloWordInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 }
